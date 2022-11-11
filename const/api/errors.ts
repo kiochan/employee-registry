f162df9fe33506a617b70edad08059774c3d779b
kiochan <sunxingchen@live.com>
2022-11-11 14:05:33 +0100

    feat: 🎸 create standard error message

---
+++ const/api/errors.ts
@@ -0,0 +1,19 @@
+import { HttpStatusCode } from './http-status-code'
+
+export const badRequest = {
+  code: HttpStatusCode.BadRequest,
+  status: 'error',
+  message: 'Bad Request: The server cannot understand your request',
+} as const
+
+export const unauthorized = {
+  code: HttpStatusCode.Unauthorized,
+  status: 'error',
+  message: 'Unauthorized: You need a token to use this service',
+} as const
+
+export const notFound = {
+  code: HttpStatusCode.NotFound,
+  status: 'error',
+  message: 'Not Found: Resources not exist',
+} as const
