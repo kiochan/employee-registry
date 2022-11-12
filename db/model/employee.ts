14b7f6f9f976beeb8c3f826b32bd027988a409c1
kiochan <sunxingchen@live.com>
2022-11-12 13:40:12 +0100

    fix: 🐛 model should only create once

--- db/model/employee.ts
+++ db/model/employee.ts
@@ -1,9 +1,9 @@
 import type { IEmployee } from '../schema/employee'
 
-import { model } from 'mongoose'
+import { model, models } from 'mongoose'
 import { EmployeeSchema } from '../schema/employee'
 
 /**
  * model for employee collection
  */
-export const EmployeeModel = model<IEmployee>('Employee', EmployeeSchema)
+export const EmployeeModel = models.Employee ?? model<IEmployee>('Employee', EmployeeSchema)
