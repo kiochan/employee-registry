deb303ab7417b99dc313e677fb7e4bae4b5aa44c
kiochan <sunxingchen@live.com>
2022-11-12 22:46:35 +0100

    feat: 🎸 a fail response should not return 200
    return Fobidden instead

--- const/api/token.ts
+++ const/api/token.ts
@@ -1,13 +1,13 @@
 import { HttpStatusCode } from './http-status-code'
 
 export const usernamePasswordMismatch = {
-  code: HttpStatusCode.Ok,
+  code: HttpStatusCode.Forbidden,
   status: 'fail',
   message: 'username and password do not match.',
 } as const
 
 export const usernameNotFound = {
-  code: HttpStatusCode.Ok,
+  code: HttpStatusCode.Forbidden,
   status: 'fail',
   message: 'username dose not exist.',
 } as const
