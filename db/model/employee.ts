6262a67f3676d9622dc5d1d43f0ffd5c4e3a45b8
kiochan <sunxingchen@live.com>
2022-11-13 15:02:42 +0100

    fix: 🐛 correct typings

--- db/model/employee.ts
+++ db/model/employee.ts
@@ -3,7 +3,9 @@ import type { IEmployee } from '../schema/employee'
 import { model, models } from 'mongoose'
 import { EmployeeSchema } from '../schema/employee'
 
+export const _model = model<IEmployee>('Employee', EmployeeSchema)
+
 /**
  * model for employee collection
  */
-export const EmployeeModel = models.Employee ?? model<IEmployee>('Employee', EmployeeSchema)
+export const EmployeeModel = (models.Employee as typeof _model) ?? _model
--- db/model/token.ts
+++ db/model/token.ts
@@ -2,7 +2,10 @@ import type { IToken } from '../schema/token'
 
 import { model, models } from 'mongoose'
 import { TokenSchema } from '../schema/token'
+
+const _model = model<IToken>('Token', TokenSchema)
+
 /**
  * model for employee collection
  */
-export const TokenModel = models.Token ?? model<IToken>('Token', TokenSchema)
+export const TokenModel = (models.Token as typeof _model) ?? _model
