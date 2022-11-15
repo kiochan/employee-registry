ac5dee161fbc9cc12144d7179be1ebecd2af4af0
kiochan <sunxingchen@live.com>
2022-11-15 09:15:15 +0100

    feat: 🎸 show login button if user is not login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
+import { useRouter } from 'next/router'
 import AppContainer from '../components/AppContainer'
+import links from '../config/links'
 import words from '../config/words'
+import { useAppSelector } from '../hooks/useAppSelector'
 
 const HomePage: React.FC = () => {
-  return <AppContainer title={words.site.titles.home} buttonLink='/login'></AppContainer>
+  const token = useAppSelector(s => s.token.value)
+  const router = useRouter()
+
+  console.log({ token })
+
+  if (token !== null) {
+    router.push(links.myself).catch(console.error)
+  }
+
+  return <AppContainer title={words.site.name} buttonLink={links.login} buttonText={words.site.titles.login} subtitle={words.text.loginRequired}></AppContainer>
 }
 
 export default HomePage
